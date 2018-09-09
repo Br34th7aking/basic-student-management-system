517a5605a97c10d1d990f3d9a507e362b94693e7
Abhijit Raj <br34th7aking@Abhijits-MacBook-Air.local>
2018-09-09 17:15:08 +0530

    Move class 'Course' code to 'course.py' module.

---
+++ course.py
@@ -0,0 +1,57 @@
+class Course:
+    def __init__(self, name="", instructor="", credits=4, preReq="", details = "", teeA=[], textbook="" \
+    , evaluation="", numStudents=0, status="", cost=0, courseId=""):
+        self.name = name
+        self.instructor = instructor
+        self.credits = credits
+        self.preReq = preReq
+        self.teeA = teeA
+        self.textbook = textbook
+        self.evaluation = evaluation
+        self.numStudents = numStudents
+        self.status = status
+        self.details = details
+        self.cost = cost
+        self.courseId = courseId
+
+    #setter methods
+    def setBasicInfo(self, name, credits, preReq, textbook, courseId):
+        self.name = name
+        self.credits = credits
+        self.preReq = preReq
+        self.textbook = textbook
+        self.courseId = courseId
+
+    def setInstTA(self, inst, teeA):
+        self.instructor = inst
+        self.teeA = teeA
+
+    def setDetails(self, details):
+        self.details = details
+
+    def setEvaluation(self, evaluation):
+        self.evaluation = evaluation
+
+    def setCost(self, cost):
+        self.cost = cost
+    # getter methods
+    def getCourseInfo(self):
+        print("*" * 20 + "\n" + \
+        "Course Name:", self.name + "\n" + \
+        "_" * 20 + "\n" + \
+        "Course ID:", self.courseId + "\n" + \
+        "_" * 20 + "\n" + \
+        "Instructor:", self.instructor + "\n" + \
+        "_" * 20 + "\n" + \
+        "Credits:", str(self.credits) + "\n" + \
+        "_" * 20 + "\n" + \
+        "Pre-requisites:", self.preReq + "\n" + \
+        "_" * 20 + "\n" + \
+        "Textbook:", self.textbook + "\n" + \
+        "_" * 20 + "\n" + \
+        "Details:", self.details + "\n" + \
+        "_" * 20 + "\n" + \
+        "Evaluation:", self.evaluation + "\n" + \
+        "_" * 20 + "\n" + \
+        "Cost: ", str(self.cost) + "\n" + \
+        "*" * 20)
--- coursedata.py
+++ coursedata.py
@@ -21,30 +21,65 @@ End-sem    30
 Attendance    5""",
 "textbook": "Algorithm Design by Kleinberg & Tardos",
 "preReq":"Introduction to Programming",
-"credits": 4
+"credits": 4,
+"courseId": "BIO522"
 },
 {
-"name":,
-"inst":,
-"teeA":,
-"cost":,
-"details":,
-"evaluation":,
-"text":,
-"prereq":,
-"cost":,
-}
+"name":"Foundation of Modern Biology",
+"instructor": "Vibhor Kumar",
+"teeA": "No TA for this course",
+"cost": 30000,
+"details": """The aim of this basic core course is to provide students a good background in genetics, molecular biology and using next generation sequencing data.  The major focus is to
+
+Cover most important concepts in genetics like Mendelian laws, linkage and recombination
+Cover  molecular mechanisms of gene expression, post transcriptional regulation
+Solve qualitative and quantitative problems in Genomics using next generation data such as ChIP-seq, RNA-seq, Dnase-seq.
+""",
+"evaluation":"""Mid-sem Exam : 25
+End-sem Exam : 25
+Assignments : 20
+Quiz : 15
+Project : 15""",
+"textbook":" Genetics: A conceptual approach by B.Pierce",
+"preReq":"Basic Statistics",
+"credits": 4,
+"courseId": "BIO512"
+},
+{
+"name":"Object Oriented Programming and Design in Python",
+"instructor":"Shachi Sharma",
+"teeA": ["Ojasvi", "Prerna", "Khusboo", "Arjun", "Rohit"],
+"cost": 15000,
+"details":"""It is a 2 credit postgraduate level core course in object oriented programming,
+designed to improve programming skills, and ability to use contemporary software development
+practices and tools. The course curriculum focuses on large software development, collaborative development
+environment, automatic testing and deployment, common libraries and continuous integration.
+It also aims to enhance the quality of design in terms of scalability and maintainability with design
+principles and design patterns.  Along with course, students get an opportunity to learn development tools
+like IDEs and common frameworks.""",
+"evaluation":"""Home Assignments/Quizzes : 5%
+Lab : 10%
+Mid Term : 30%
+End Term : 40%
+Project : 15%""",
+"textbook":"Python3 Object Oriented Programming by Dusty Phillips",
+"preReq":"CSE101, CSE102",
+"credits":2,
+"courseId": "CSE600"
+},
+
 ]
 
 
 # {
 # "name":,
-# "inst":,
+# "instructor":,
 # "teeA":,
 # "cost":,
 # "details":,
 # "evaluation":,
-# "text":,
-# "prereq":,
-# "cost":,
+# "textbook":,
+# "preReq":,
+# "credits":,
+# "courseId":
 # }
